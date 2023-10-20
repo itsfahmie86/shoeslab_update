0e731044e757b7ef055cb02282e425186444e008
itsfahmie <itsfahmi86@gmail.com>
2023-10-20 22:06:44 +0700

    update friday

--- include/navigation.php
+++ include/navigation.php
@@ -33,29 +33,15 @@
                 <!-- <li class="nav-item">
                         <a class="nav-link" href="status">Status</a>
                     </li> -->
-                <!-- <li class="nav-item">
+                <li class="nav-item">
                         <a class="nav-link" href="blog"> Blogs</a>
-                    </li> -->
+                    </li>
                 <li class="nav-item">
                     <a class="nav-link" href="contact">
                         Contact
                     </a>
                 </li>
             </ul>
-
-            <!-- <div class="nav-info-right d-flex align-items-center">
-                <div class="social">
-                    <ul class="rest">
-                        <li>
-                            <a href="https://www.instagram.com/_shoeslab/"><i class="fab fa-instagram"></i></a>
-                            <a href="https://www.youtube.com/@shoeslab"><i class="fab fa-youtube"></i></a>
-                            <a href="https://www.tiktok.com/@shoeslab_?lang=ar"><i class="fab fa-tiktok"></i></a>
-                            <a href="https://id.pinterest.com/shoeslab27/"><i class="fab fa-pinterest"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div> -->
-
         </div>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
