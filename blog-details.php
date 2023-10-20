405a416d56e5ddd8a44ac3b60a8d72ebfa1f66b1
itsfahmie <itsfahmi86@gmail.com>
2023-10-20 22:06:44 +0700

    update friday

---
+++ blog-details.php
@@ -0,0 +1,307 @@
+<?php
+include 'include/header.php';
+include 'include/navigation.php';
+?>
+
+<main>
+
+        <!-- ==================== Start Slider ==================== -->
+
+        <header class="pg-header tpost bg-img parallaxie valign" data-background="assets/img/blog/b/p2.jpg"
+            data-overlay-dark="3">
+            <div class="container ontop">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="cont text-center mb-80">
+                            <h1 class="fw-700 mb-50">Creative advertising in <br> our life became a info noise</h1>
+                            <div class="d-flex justify-content-center">
+                                <div class="d-flex align-items-center">
+                                    <div class="item mr-30">
+                                        <div class="d-flex align-items-center">
+                                            <div class="f-img">
+                                                <div class="img circle-60 mr-20">
+                                                    <img src="assets/img/blog/a1.jpg" alt="" class="circle-img">
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <span><a href="blog-masonry-3col.html">By : Alex Martin</a></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="item mr-30">
+                                        <span><a href="blog-masonry-3col.html"> 30 august 2021</a></span>
+                                    </div>
+                                    <div class="item">
+                                        <span><a href="blog-masonry-3col.html"> WordPress</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="shadw"></div>
+        </header>
+
+        <!-- ==================== End Slider ==================== -->
+
+
+
+        <!-- ==================== Start Blog ==================== -->
+
+        <section class="blog section-padding pb-0">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-11">
+                        <div class="main-post">
+                            <div class="item pb-60">
+
+                                <div class="text-center">
+                                    <div class="row justify-content-center">
+                                        <div class="col-lg-10">
+                                            <div class="title">
+                                                <h4 class="fw-700">Apple’s leaked 15-inch MacBook Air might not be an
+                                                    Air after
+                                                    all.</h4>
+                                            </div>
+                                            <div class="text mt-20">
+                                                <p>A new report said earlier this week that Apple is working on a brand
+                                                    new laptop.
+                                                    Apple plans to release a 15-inch MacBook Air in 2023, a first for
+                                                    the Air
+                                                    series. A trusted Apple insider with a proven track record confirmed
+                                                    that Apple
+                                                    is working on the larger MacBook Air.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="main-img mt-40 mb-40 radius-5">
+                                        <img src="assets/img/blog/b/b1.jpg" alt="">
+                                    </div>
+                                    <div class="row justify-content-center">
+                                        <div class="col-lg-10">
+                                            <div class="text">
+                                                <p>However, Apple might not include it in the Air series when it
+                                                    launches it. As for
+                                                    the notebook’s release date, the 15-inch MacBook isn’t coming soon.
+                                                    It’ll get a
+                                                    late 2023 release at best, according to the new claims.</p>
+                                            </div>
+                                            <div class="title mt-30">
+                                                <h5 class="fw-600">What sizes do MacBook Airs come in?</h5>
+                                            </div>
+                                            <div class="text mt-20">
+                                                <p>Apple currently sells only one MacBook Air size. The laptop comes in
+                                                    a 13-inch
+                                                    version that matches the pre-2021 13-inch MacBook Pro size.
+                                                    Previously, Apple
+                                                    sold an 11-inch MacBook Air, but the company discontinued that model
+                                                    in 2017.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row justify-content-center">
+                                    <div class="col-lg-10">
+                                        <div class="post-qoute mt-50">
+                                            <h6 class="fz-20">
+                                                <span class="l-block">Increase your site traffic and gain new customers with a beautiful
+                                                    and
+                                                    functional blog.</span>
+                                                <span class="sub-title main-color mt-20 mb-0"> - Cinzel Universe</span>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="mb-50 mt-50">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="iner-img">
+                                                <img src="assets/img/blog/h1.jpg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="iner-img">
+                                                <img src="assets/img/blog/h2.jpg" alt="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row justify-content-center">
+                                    <div class="col-lg-10">
+                                        <div class="title mb-30">
+                                            <h5 class="fw-600">Apple currently sells only one MacBook Air size.</h5>
+                                        </div>
+
+                                        <div class="text mb-20">
+                                            <p>A new report said earlier this week that Apple is working on a brand new
+                                                laptop.
+                                                Apple plans to release a 15-inch MacBook Air in 2023, a first for the
+                                                Air
+                                                series. A trusted Apple insider with a proven track record confirmed
+                                                that Apple
+                                                is working on the larger MacBook Air.</p>
+                                        </div>
+
+                                        <div class="unorder-list mb-30">
+                                            <h6 class="fw-600 mb-15">Ordered & Unordered Lists.</h6>
+                                            <ul class="rest">
+                                                <li>Yet this above sewed flirted opened ouch</li>
+                                                <li>Goldfinch realistic sporadic ingenuous</li>
+                                                <li>Abominable this abidin far successfully then like piquan</li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="order-list mb-30">
+                                            <h6 class="fw-600 mb-15">Ordered & Unordered Lists.</h6>
+                                            <ul class="rest">
+                                                <li><span>01 -</span> Yet this above sewed flirted opened ouch</li>
+                                                <li><span>02 -</span> Goldfinch realistic sporadic ingenuous</li>
+                                                <li><span>03 -</span> Abominable this abidin far successfully then like
+                                                    piquan
+                                                </li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="text">
+                                            <p>However, Apple might not include it in the Air series when it launches
+                                                it. As for
+                                                the notebook’s release date, the 15-inch MacBook isn’t coming soon.
+                                                It’ll get a
+                                                late 2023 release at best, according to the new claims.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="info-area flex mt-20 pb-20">
+                                <div>
+                                    <div class="tags flex">
+                                        <div class="valign">
+                                            <span>Tags :</span>
+                                        </div>
+                                        <div>
+                                            <a href="blog-grid-clean.html">Tech</a>
+                                            <a href="blog-grid-clean.html">Ravo</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="ml-auto">
+                                    <div class="share-icon flex">
+                                        <div class="valign">
+                                            <span>Share :</span>
+                                        </div>
+                                        <div>
+                                            <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                                            <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+                                            <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="next-prv-post flex mt-50">
+                                <div class="thumb-post bg-img" data-background="assets/img/blog/b/b4.jpg">
+                                    <a href="blog-post.html">
+                                        <span class="fz-12 text-u ls1 main-color mb-15"><i class="pe-7s-angle-left"></i>
+                                            Prev Post</span>
+                                        <h6 class="fw-600 fz-16">Ways to quickly traffic to <br> your website.</h6>
+                                    </a>
+                                </div>
+                                <div class="thumb-post ml-auto text-right bg-img"
+                                    data-background="assets/img/blog/b/b5.jpg">
+                                    <a href="blog-post.html">
+                                        <span class="fz-12 text-u ls1 main-color mb-15">Next Post <i
+                                                class="pe-7s-angle-right"></i></span>
+                                        <h6 class="fw-600 fz-16">How to Handle Your Good Employee.</h6>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="recent-posts blog-overlay sub-bg section-padding mt-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="mb-60">
+                                <h4>Recent Posts</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="item md-mb50">
+                                <div class="img">
+                                    <img src="assets/img/blog/b/b9.jpg" alt="">
+                                    <div class="cont">
+                                        <div class="cont">
+                                            <div class="date sub-title fw-500 opacity-8">
+                                                <a href="blog-masonry-3col.html">Design / 30 august 2021</a>
+                                            </div>
+                                            <h6 class="fz-20 fw-700">
+                                                <a href="blog-post.html">Creative advertising in our life became a
+                                                    info
+                                                    noise</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="item md-mb50">
+                                <div class="img">
+                                    <img src="assets/img/blog/b/b10.jpg" alt="">
+                                    <div class="cont">
+                                        <div class="cont">
+                                            <div class="date sub-title fw-500 opacity-8">
+                                                <a href="blog-masonry-3col.html">Design / 30 august 2021</a>
+                                            </div>
+                                            <h6 class="fz-20 fw-700">
+                                                <a href="blog-post.html">Creative advertising in our life became a
+                                                    info
+                                                    noise</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="item">
+                                <div class="img">
+                                    <img src="assets/img/blog/b/b14.jpg" alt="">
+                                    <div class="cont">
+                                        <div class="cont">
+                                            <div class="date sub-title fw-500 opacity-8">
+                                                <a href="blog-masonry-3col.html">Design / 30 august 2021</a>
+                                            </div>
+                                            <h6 class="fz-20 fw-700">
+                                                <a href="blog-post.html">Creative advertising in our life became a
+                                                    info
+                                                    noise</a>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ==================== End Blog ==================== -->
+
+
+    </main>
+
+    <?php
+    include 'include/footer.php';
+?>
