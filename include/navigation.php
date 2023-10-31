076591e01d62e1f9d05dcc4a541d313b5e3c93e1
itsfahmie86 <itsfahmi86@gmail.com>
2023-10-31 13:01:21 +0700

    .

--- include/navigation.php
+++ include/navigation.php
@@ -27,10 +27,6 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="product">Produk </a>
                 </li>
-                <!-- <li class="nav-item dropdown">
-                <!-- <li class="nav-item dropdown">
-                        <a class="nav-link" href="gallery">Galeri </a>
-                    </li> -->
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="gallery">Gallery </a>
                 </li>
