c83ca6f2f20f1cf2981823c26af2b22e9a07a237
itsfahmie86 <itsfahmi86@gmail.com>
2023-10-31 10:59:56 +0700

    edit nav

--- include/navigation.php
+++ include/navigation.php
@@ -27,7 +27,6 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="product">Produk </a>
                 </li>
-<<<<<<< HEAD
                 <!-- <li class="nav-item dropdown">
                 <!-- <li class="nav-item dropdown">
                         <a class="nav-link" href="gallery">Galeri </a>
@@ -35,21 +34,9 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="gallery">Gallery </a>
                 </li>
-=======
-<<<<<<< HEAD
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="gallery">Gallery </a>
-                </li>
-=======
-                <!-- <li class="nav-item dropdown">
-                <!-- <li class="nav-item dropdown">
-                        <a class="nav-link" href="gallery">Galeri </a>
-                    </li> -->
->>>>>>> be798ab (l)
->>>>>>> 1f8e59e (l)
-                <!-- <li class="nav-item">
+                <li class="nav-item">
                         <a class="nav-link" href="blog"> Blogs</a>
-                    </li> -->
+                    </li>
                 <li class="nav-item">
                     <a class="nav-link" href="contact">
                         Contact
